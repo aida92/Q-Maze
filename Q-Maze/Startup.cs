166067bbd8856b29b89d8691532aaf2fde4169e2
aida92 <aidazolic@hotmail.com>
2016-03-08 22:04:07 +0100

    First commit - Initial MVC

---
+++ Q-Maze/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Q_Maze.Startup))]
+namespace Q_Maze
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
