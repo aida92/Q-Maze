b76ef62c24dea51696d44442989272feb7aa82a3
aida92 <aidazolic@hotmail.com>
2016-08-23 08:24:09 +0200

    Trying somehing, and added a few links

---
+++ Q-Maze/Controllers/StatisticsController.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using QMaze.Models;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.Owin.Security;
+
+namespace QMaze.Controllers
+{
+    
+    public class StatisticsController : Controller
+    {
+        public ApplicationUser AppUser
+        {
+            get
+            {
+                UserManager<ApplicationUser> manager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(new ApplicationDbContext()));
+                return manager.FindByName(User.Identity.Name);
+            }
+        }
+        
+
+        // GET: Statistics
+        public ActionResult Index()
+        {
+            GameStatisticsModel model = new GameStatisticsModel();
+            return View(model);
+        }
+
+        public ActionResult PlayerStatistics(string Id)
+        {
+            if (!User.Identity.IsAuthenticated)
+                return RedirectToAction("Index");
+            if (Id == null || Id == "")
+            {
+                Id = AppUser.Id;
+            }
+            UserStatisticsModel model = new UserStatisticsModel(Id);
+            return View(model);
+        }
+
+        [HttpPost]
+        public ActionResult RefreshList(GameStatisticsModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                model.RefreshList();
+            }
+            return PartialView("_PlayerList", model);
+        }
+
+        public ActionResult Error(string Message)
+        {
+            ViewBag.Message = Message;
+            return View("~/Views/Shared/Error.cshtml");
+        }
+    }
+}
---
+++ Q-Maze/Scripts/return.js
@@ -0,0 +1,15 @@
+﻿$(".returnlink").click(function (event) {
+    event.preventDefault();
+    var link = this;
+    // $(link).text($(link).attr("href"));
+    $.ajax($(link).attr("href"),
+        {
+            method: "POST",
+            data: { id: $(link).attr("data-id") },
+            success: function (data, status, xhr) {
+                if (data.Success === true)
+                    $(link).parents(".leasedays").html(data.LeaseDays + " day(s)");
+            },
+            dataType: "json"
+        });
+});
